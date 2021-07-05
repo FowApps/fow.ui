608fd6182209943a2705fdeaf671401b3b7a4277
Görkem Tosun <grkkmm1@gmail.com>
2021-07-05 21:59:36 +0300

    add exports

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,20 @@
 import Heading from './components/atoms/Heading';
 import SubTitle from './components/atoms/Subtitle';
 import Body from './components/atoms/Body';
+import Overline from './components/atoms/Overline';
+import Caption from './components/atoms/Caption';
 
 export const Typography = {
     Heading,
     SubTitle,
     Body,
+    Overline,
+    Caption,
 };
 
 export { default as Avatar } from './components/atoms/Avatar';
 export { default as Icon } from './components/atoms/Icon';
+export { default as ColoredIcon } from './components/atoms/ColoredIcon';
 export { default as Checkbox } from './components/atoms/Checkbox';
 export { default as Space } from './components/atoms/Space';
 export { default as Chip } from './components/atoms/Chip';
