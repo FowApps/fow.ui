adbb4062bba70b97d001f154f80effe30c4e6aa2
Görkem Tosun <grkkmm1@gmail.com>
2021-07-07 19:26:51 +0300

    add loader component

---
+++ src/components/atoms/Loader/styles/Loader.style.ts
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+import LoadingOverlay, { LoadingOverlayProps } from 'react-loading-overlay';
+
+interface OverlayProps extends LoadingOverlayProps {
+    fullPage?: boolean;
+}
+
+interface StaticPlaceholderProps {
+    height?: number;
+}
+
+export const StyledLoader = styled(LoadingOverlay)<OverlayProps>`
+    &.fow-loader-overlay_wrapper {
+        height: 100%;
+    }
+    .fow-loader-overlay_overlay {
+        position: ${(props) => (props.fullPage ? 'fixed' : 'absolute')};
+        background: rgba(255, 255, 255, 0.75);
+    }
+    .fow-loader-overlay_content {
+        color: ${(props) => props.theme.fow.colors.primary.dark} !important;
+    }
+    .fow-loader-overlay_spinner {
+        width: 2.5rem;
+        margin-bottom: ${(props) =>
+            props.text ? props.theme.fow.spacing.small : 0};
+        svg circle {
+            stroke: ${(props) => props.theme.fow.colors.primary.dark};
+        }
+    }
+`;
+
+export const StaticPlaceholder = styled.div<StaticPlaceholderProps>`
+    height: ${(props) => props.height || 0}px;
+    background-color: transparent !important;
+`;
