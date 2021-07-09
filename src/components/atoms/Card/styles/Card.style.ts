ad10826c247a61feec01bbf0ddb092dacee78bf4
Görkem Tosun <grkkmm1@gmail.com>
2021-07-09 17:20:00 +0300

    add active props to card component

--- src/components/atoms/Card/styles/Card.style.ts
+++ src/components/atoms/Card/styles/Card.style.ts
@@ -1,9 +1,17 @@
 import styled from 'styled-components';
 
-export const StyledCard = styled.div`
+type CardProps = {
+    isActive: boolean;
+};
+
+export const StyledCard = styled.div<CardProps>`
     box-sizing: border-box;
     padding: ${(props) => props.theme.fow.spacing.medium};
     background-color: ${(props) => props.theme.fow.colors.common.white};
-    border: 1px solid ${(props) => props.theme.fow.colors.grey.transparent16};
+    border: 1px solid
+        ${(props) =>
+            props.isActive
+                ? props.theme.fow.colors.primary.light
+                : props.theme.fow.colors.grey.transparent16};
     border-radius: 0.8rem;
 `;
