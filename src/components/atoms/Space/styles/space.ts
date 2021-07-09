32edba5bb20bb94dedfba8934056281a14958e7a
Görkem Tosun <grkkmm1@gmail.com>
2021-07-09 17:43:20 +0300

    fix space margin bottom issue

--- src/components/atoms/Space/styles/space.ts
+++ src/components/atoms/Space/styles/space.ts
@@ -37,11 +37,14 @@ export const setSpaceAndDirection = (
                 flex-direction: ${reverse ? 'row-column' : 'column'};
                 > *:not(:last-child) {
                     ${reverse
-                        ? `margin-top: ${(props) =>
-                              props.theme.fow.spacing[size]};`
-                        : `margin-bottom: ${(props) =>
-                              props.theme.fow.spacing[size]};`};
-                    margin-bottom: ${(props) => props.theme.fow.spacing[size]};
+                        ? css`
+                              margin-top: ${(props) =>
+                                  props.theme.fow.spacing[size]};
+                          `
+                        : css`
+                              margin-bottom: ${(props) =>
+                                  props.theme.fow.spacing[size]};
+                          `};
                 }
             `;
         default:
@@ -49,10 +52,14 @@ export const setSpaceAndDirection = (
                 flex-direction: ${reverse ? 'row-reverse' : 'row'};
                 > *:not(:last-child) {
                     ${reverse
-                        ? `margin-left: ${(props) =>
-                              props.theme.fow.spacing[size]};`
-                        : `margin-right: ${(props) =>
-                              props.theme.fow.spacing[size]};`};
+                        ? css`
+                              margin-left: ${(props) =>
+                                  props.theme.fow.spacing[size]};
+                          `
+                        : css`
+                              margin-right: ${(props) =>
+                                  props.theme.fow.spacing[size]};
+                          `};
                 }
             `;
     }
