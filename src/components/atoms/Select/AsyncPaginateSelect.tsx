9084b02d145300efdafa842149486eef2e028165
Görkem Tosun <grkkmm1@gmail.com>
2021-07-25 16:14:39 +0300

    finishc async paginate select component

--- src/components/atoms/Select/AsyncPaginateSelect.tsx
+++ src/components/atoms/Select/AsyncPaginateSelect.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { forwardRef, RefObject } from 'react';
 import { AsyncPaginate } from 'react-select-async-paginate';
+
+import Space from '../Space';
+import Caption from '../Typography/Caption';
 
 import { BaseSelectProps } from './StaticSelect';
 import Loader from '../Loader';
 
-import { theme } from '../../../theme/theme';
+import {
+    renderControlStyles,
+    themeColors,
+    Wrapper,
+    Label,
+    ValidationMessage,
+} from './styles';
 
 type Additional = {
     page: number;
@@ -40,27 +49,33 @@ export interface AsyncPaginateSelectProps extends BaseSelectProps {
     loadingMessage?: string;
 }
 
-const AsyncPaginateSelect = ({
-    placeholder,
-    isMulti = false,
-    isSearchable = false,
-    isDisabled = false,
-    isClearable = false,
-    closeMenuOnSelect = true,
-    valueKey = 'value',
-    labelKey = 'label',
-    cacheOptions = true,
-    defaultOptions = true,
-    debounceTime = 500,
-    loadingMessage = 'Loading',
-    loadOptions,
-    onChange,
-    ...rest
-}: AsyncPaginateSelectProps): JSX.Element => {
+const AsyncPaginateSelect = (
+    {
+        placeholder,
+        isMulti = false,
+        isSearchable = false,
+        isDisabled = false,
+        isClearable = false,
+        closeMenuOnSelect = true,
+        valueKey = 'value',
+        labelKey = 'label',
+        cacheOptions = true,
+        defaultOptions = true,
+        debounceTime = 500,
+        loadingMessage = 'Loading',
+        loadOptions,
+        onSelect,
+        onChange,
+        error,
+        label,
+        required,
+        ...rest
+    }: AsyncPaginateSelectProps,
+    ref: RefObject<any>,
+): JSX.Element => {
     const handleChange = (option: any) => {
-        if (typeof onChange === 'function') {
-            onChange(option);
-        }
+        if (typeof onChange === 'function') onChange(option);
+        if (typeof onSelect === 'function') onSelect(option);
     };
 
     const LoadingMessage = (props: any) => (
@@ -72,50 +87,59 @@ const AsyncPaginateSelect = ({
     );
 
     return (
-        <AsyncPaginate
-            theme={(defaultTheme) => ({
-                ...defaultTheme,
-                borderRadius: 8,
-                colors: {
-                    ...defaultTheme.colors,
-                    primary25: theme.fow.colors.primary.transparent12,
-                    primary50: theme.fow.colors.primary.lighter,
-                    primary75: theme.fow.colors.primary.light,
-                    primary: theme.fow.colors.primary.main,
-                    danger: theme.fow.colors.error.main,
-                    dangerLight: theme.fow.colors.error.light,
-                },
-            })}
-            styles={{
-                control: (styles) => ({
-                    ...styles,
-                    transition: 'all 0.3s ease',
-                    minHeight: '4rem',
+        <Wrapper>
+            {label && (
+                <Space size="xxsmall">
+                    {required && <Caption color="error">*</Caption>}
+                    <Label>{label}</Label>
+                </Space>
+            )}
+            <AsyncPaginate
+                {...rest}
+                ref={ref}
+                theme={(defaultTheme) => ({
+                    ...defaultTheme,
                     borderRadius: 8,
-                    fontSize: '16px',
-                    lineHeight: '24px',
-                }),
-            }}
-            debounceTimeout={debounceTime}
-            cacheOptions={cacheOptions}
-            defaultOptions={defaultOptions}
-            loadOptions={loadOptions}
-            getOptionValue={(option) => option[valueKey]}
-            getOptionLabel={(option) => option[labelKey]}
-            onChange={handleChange}
-            isMulti={isMulti}
-            isClearable={isClearable}
-            isDisabled={isDisabled}
-            isSearchable={isSearchable}
-            placeholder={placeholder}
-            closeMenuOnSelect={closeMenuOnSelect}
-            components={{ LoadingMessage }}
-            additional={{
-                page: 1,
-            }}
-            {...rest}
-        />
+                    colors: {
+                        ...defaultTheme.colors,
+                        ...themeColors,
+                    },
+                })}
+                styles={{
+                    control: (styles, { isFocused }) => ({
+                        ...styles,
+                        ...{ ...renderControlStyles(isFocused, !!error) },
+                    }),
+                    menu: (styles) => ({
+                        ...styles,
+                        top: 'calc(100% - 2.4rem)',
+                    }),
+                }}
+                debounceTimeout={debounceTime}
+                cacheOptions={cacheOptions}
+                defaultOptions={defaultOptions}
+                loadOptions={loadOptions}
+                getOptionValue={(option) => option[valueKey]}
+                getOptionLabel={(option) => option[labelKey]}
+                onChange={handleChange}
+                isMulti={isMulti}
+                isClearable={isClearable}
+                isDisabled={isDisabled}
+                isSearchable={isSearchable}
+                placeholder={placeholder}
+                closeMenuOnSelect={closeMenuOnSelect}
+                components={{ LoadingMessage }}
+                additional={{
+                    page: 1,
+                }}
+            />
+            {error && (
+                <ValidationMessage color="error">
+                    {error.message}
+                </ValidationMessage>
+            )}
+        </Wrapper>
     );
 };
 
-export default AsyncPaginateSelect;
+export default forwardRef(AsyncPaginateSelect);
