6b9a20803e6f703d1ff7a5a51b8288cae5617c53
cagatay kaban <mcagataykaban@gmail.com>
2021-08-11 14:14:11 +0300

    💄 change request add padding x-axis

--- src/components/atoms/Label/styles/Label.style.ts
+++ src/components/atoms/Label/styles/Label.style.ts
@@ -14,13 +14,14 @@ type LabelProps = {
 export const StyledLabel = styled.div<LabelProps>`
     display: inline-flex;
     align-items: center;
+    padding-right: 1.1rem;
+    padding-left: 1.1rem;
     color: white;
     font-weight: 700;
     font-size: 1.2rem;
     font-style: normal;
     line-height: 2rem;
     border-radius: ${(props) => (props.shape === 'flat' ? 0 : 0.6)}rem;
-
     ${(props) => setVariant(props.color, props.variant)}
     ${(props) => setSize(props.size)}
     ${(props) => setTextTransform(props.textTransfrom)}
