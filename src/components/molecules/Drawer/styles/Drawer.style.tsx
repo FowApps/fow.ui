017fd2f3195245f6cfb641d9c99bb08975eb9289
grkmtsn <grkkmm1@gmail.com>
2021-09-17 17:02:33 +0300

    add drawer core styles

---
+++ src/components/molecules/Drawer/styles/Drawer.style.tsx
@@ -0,0 +1,38 @@
+import styled, { css } from 'styled-components';
+
+interface ContainerProps {
+    isDestroyOnClose: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    width: 100%;
+    height: 100%;
+
+    ${(props) =>
+        props.isDestroyOnClose &&
+        css`
+            opacity: 0;
+            transition: opacity 0.3s;
+        `}
+`;
+
+export const Header = styled.header`
+    padding: ${(props) => props.theme.fow.spacing.small};
+    border-bottom: 1px solid ${(props) => props.theme.fow.colors.divider};
+`;
+
+export const Body = styled.div`
+    flex-grow: 1;
+    overflow: auto;
+    padding: ${(props) => props.theme.fow.spacing.small};
+    word-wrap: break-word;
+`;
+
+export const Footer = styled.footer`
+    flex-shrink: 0;
+    padding: ${(props) => props.theme.fow.spacing.small};
+    border-top: 1px solid ${(props) => props.theme.fow.colors.divider};
+`;
