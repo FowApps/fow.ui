11c95ccdc6223138aafbc24b3196b464bfb45506
grkmtsn <grkkmm1@gmail.com>
2021-10-01 16:30:25 +0300

    fix carousel arrow and dot issue

--- src/components/molecules/Carousel/Carousel.tsx
+++ src/components/molecules/Carousel/Carousel.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+/* eslint-disable no-return-assign */
+import React, { useRef } from 'react';
+import Slider from 'react-slick';
 
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import { StyledSlider } from './styles';
+import { StyledSlider, Pagination, Arrow, Dot } from './styles';
 
 export interface CarouselProps {
     /**
@@ -36,16 +38,42 @@ const Carousel = ({
     slidesToScroll = 1,
     children,
     ...rest
-}: CarouselProps): JSX.Element => (
-    <StyledSlider
-        dots={dots}
-        infinite={infinite}
-        speed={speed}
-        slidesToShow={slidesToShow}
-        slidesToScroll={slidesToScroll}
-        {...rest}>
-        {children}
-    </StyledSlider>
-);
+}: CarouselProps): JSX.Element => {
+    const slider = useRef<Slider>();
+
+    const settings = {
+        dots,
+        infinite,
+        speed,
+        slidesToShow,
+        slidesToScroll,
+        arrows: false,
+        appendDots: (allDots) => (
+            <Pagination>
+                <Arrow
+                    icon="chevron-left"
+                    onClick={() => {
+                        slider.current?.slickPrev();
+                    }}
+                />{' '}
+                <div>{allDots}</div>
+                <Arrow
+                    icon="chevron-right"
+                    onClick={() => {
+                        slider.current?.slickNext();
+                    }}
+                />
+            </Pagination>
+        ),
+        customPaging: () => <Dot />,
+        ...rest,
+    };
+    return (
+        // @ts-ignore
+        <StyledSlider ref={slider} {...settings}>
+            {children}
+        </StyledSlider>
+    );
+};
 
 export default Carousel;
