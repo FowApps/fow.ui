e5ac88315dee49dbe70e1dc9e2f61a9b4198ed27
grkmtsn <grkkmm1@gmail.com>
2021-10-04 14:35:48 +0300

    add sidebar component

---
+++ src/components/molecules/Sidebar/styles/Sidebar.style.ts
@@ -0,0 +1,73 @@
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+
+export const COLLAPSED_WIDTH = '12px';
+export const EXPANDED_WIDTH = '200px';
+
+type SiderProps = {
+    noGutter: boolean;
+};
+
+export const SidebarVariants = {
+    expanded: () => ({
+        width: EXPANDED_WIDTH,
+        transition: {
+            when: 'beforeChildren',
+        },
+    }),
+    collapsed: () => ({
+        width: COLLAPSED_WIDTH,
+        transition: {
+            when: 'afterChildren',
+        },
+    }),
+};
+
+export const ContentVariants = {
+    expanded: {
+        opacity: 1,
+        display: 'flex',
+    },
+    collapsed: {
+        opacity: 0,
+        transitionEnd: {
+            display: 'none',
+        },
+    },
+};
+
+export const Sider = styled(motion.div)<SiderProps>`
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: space-between;
+    min-width: 0;
+    padding: ${(props) => (props.noGutter ? 0 : props.theme.fow.spacing.small)};
+    background-color: ${(props) => props.theme.fow.colors.common.white};
+    box-shadow: inset -1px 0px 0px rgba(145, 158, 171, 0.24);
+
+    &:hover button {
+        opacity: 1;
+    }
+`;
+export const Trigger = styled.button`
+    position: absolute;
+    top: 12px;
+    right: -12px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 24px;
+    height: 24px;
+    border: 1px solid rgba(145, 158, 171, 0.24);
+    border-radius: 50%;
+    background-color: ${(props) => props.theme.fow.colors.common.white};
+    color: ${(props) => props.theme.fow.colors.text.secondary};
+    outline: none;
+    cursor: pointer;
+    opacity: 0;
+    transition: opacity 0.3s ease;
+`;
+
+export const Content = styled(motion.div)``;
