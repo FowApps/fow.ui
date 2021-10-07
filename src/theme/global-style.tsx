44c2d55e11a16b4f9892b8f53ebe3e212a94d7f8
grkmtsn <grkkmm1@gmail.com>
2021-10-07 13:35:35 +0300

    change scrollbar styles

--- src/theme/global-style.tsx
+++ src/theme/global-style.tsx
@@ -25,19 +25,18 @@ const GlobalStyle = createGlobalStyle`
 
     &::-webkit-scrollbar-track {
         border-radius: 1rem;
-        background-color: #F5F5F5;
-        box-shadow: inset 0 0 0.6rem rgba(0,0,0,0.05);
+        background-color: transparent;
     }
 
     &::-webkit-scrollbar {
-        width: 1rem;
-        height: 1rem;
-        background-color: #FFF;
+        width: 0.4rem;
+        height: 0.4rem;
+        background-color: transparent;
     }
     
     &::-webkit-scrollbar-thumb {
         border-radius: 1rem;
-        background-color: ${(props) => props.theme.fow.colors.grey.main};
+        background-color: ${(props) => props.theme.fow.colors.grey.light};
     }
 
     .rc-menu-submenu-hidden {
