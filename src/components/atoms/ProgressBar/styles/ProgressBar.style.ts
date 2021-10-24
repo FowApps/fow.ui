cac2d9e6a77b7c4bdd758b12b1f3938446efb6b2
cagatay kaban <mcagataykaban@gmail.com>
2021-10-24 23:53:09 +0300

    add ProgressBar component to atoms

---
+++ src/components/atoms/ProgressBar/styles/ProgressBar.style.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+import setColor from './color';
+
+type ProgressBarProps = {
+    progress?: number;
+    color?: string;
+};
+
+export const StyledProgressBar = styled.div<ProgressBarProps>`
+    display: flex;
+    width: 100%;
+    height: 0.5rem;
+    border-radius: 10rem;
+    transition: background-color 200ms ease;
+    background: ${(props) => props.theme.fow.colors.grey.lighter};
+`;
+
+export const StyledDiv = styled.div<ProgressBarProps>`
+    width: ${(props) => props.progress}%;
+    height: 100%;
+    border-radius: 100px;
+    ${(props) => setColor(props.progress)};
+`;
