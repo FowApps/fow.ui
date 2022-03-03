528d5984d6341a8daf18686bad2085169895efd8
grkmtsn <grkkmm1@gmail.com>
2022-03-03 07:33:17 +0300

    add localization

---
+++ src/components/atoms/Form/locales/tr.ts
@@ -0,0 +1,11 @@
+export const tr = {
+    select: 'Seçiniz',
+    pickDate: 'Tarih Seçiniz',
+    startDate: 'Başlangıç Tarihi',
+    endDate: 'Bitiş Tarihi',
+    invalidUrl: 'Geçersiz URL formatı.',
+    invalidEmail: 'Geçersiz mail adresi.',
+    max255: 'Maksimum 255 karakter girilebilir.',
+    string: 'Girilen değer düz yazı olmalıdır.',
+    number: 'Girilen değer sayı olmalıdır.',
+};
