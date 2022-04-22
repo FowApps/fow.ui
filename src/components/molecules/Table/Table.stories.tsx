dc456d97fa3dcc596af973854c05af46edb6e619
grkmtsn <grkkmm1@gmail.com>
2022-04-22 18:31:15 +0300

    add uncontrolled pagination(static) for table. add row selection.

--- src/components/molecules/Table/Table.stories.tsx
+++ src/components/molecules/Table/Table.stories.tsx
@@ -8,7 +8,7 @@ export default {
     component: Table,
 } as Meta;
 
-const Template: Story<TableProps> = (args) => {
+const ManualPaginationTemplate: Story<TableProps> = (args) => {
     const [users, setUsers] = useState({
         data: [],
         total: 0,
@@ -86,8 +86,82 @@ const Template: Story<TableProps> = (args) => {
             initialPage={pagination.page}
             pageSize={pagination.pageSize}
             sortBy={[{ id: 'first_name', desc: false }]}
+            showSelection
         />
     );
 };
 
-export const Default = Template.bind({});
+export const ManualPagination = ManualPaginationTemplate.bind({});
+
+const UncontrolledPaginationTemplate: Story<TableProps> = (args) => {
+    const [users, setUsers] = useState({
+        data: [],
+        total: 0,
+    });
+    const [isLoading, setIsLoading] = useState(true);
+    const [pagination, setPagination] = useState({ page: 1, pageSize: 20 });
+
+    const columns = useMemo(
+        () => [
+            {
+                key: 'first_name',
+                Header: 'First Name',
+                accessor: 'first_name',
+                disableSortBy: false,
+            },
+            {
+                key: 'last_name',
+                Header: 'Last Name',
+                accessor: 'last_name',
+                disableSortBy: true,
+                isVisible: true,
+            },
+            {
+                key: 'email',
+                Header: 'Email',
+                accessor: 'email',
+                disableSortBy: true,
+                isVisible: true,
+            },
+        ],
+        [],
+    );
+
+    const fetchAPIData = async () => {
+        try {
+            setIsLoading(true);
+            const response = await fetch(
+                `https://reqres.in/api/users?page=${pagination.page}&per_page=${pagination.pageSize}`,
+            );
+            const json = await response.json();
+            const data = json.data.map((d) => {
+                d.disabled = true;
+                return d;
+            });
+            setUsers({
+                data,
+                total: json.total,
+            });
+        } catch (e) {
+            console.log('Error while fetching', e);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchAPIData();
+    }, [pagination]);
+
+    return (
+        <Table
+            data={users?.data || []}
+            columns={columns}
+            manualPagination={false}
+            showSelection
+            onSelectedRowChange={(rows) => console.log(rows)}
+        />
+    );
+};
+
+export const UncontrolledPagination = UncontrolledPaginationTemplate.bind({});
