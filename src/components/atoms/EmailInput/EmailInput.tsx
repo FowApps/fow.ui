9c76a7d86adf95f4cc8af47530e6b3c43ac5881d
Dogukan Tansuk <dogukantansuk@gmail.com>
2023-01-25 14:45:06 +0300

    :recycle: Refactor email component

--- src/components/atoms/EmailInput/EmailInput.tsx
+++ src/components/atoms/EmailInput/EmailInput.tsx
@@ -1,29 +1,10 @@
-import React, { forwardRef, RefObject, useEffect, useState } from 'react';
+import React, { forwardRef, RefObject, useState } from 'react';
 
 import Input, { InputProps } from '../Input';
-import Select, { Props as SelectProps } from '../Select';
 import Space from '../Space';
-import { InputWrapper, SelectWrapper, FixedWrapper } from './styles';
-import useIsMountFirstTime from '../../../hooks/useIsMountFirstTime';
-
-const { Option } = Select;
-
-interface Extension {
-    /**
-     * value of extension
-     */
-    value: string;
-    /**
-     * name of extension
-     */
-    name: string;
-}
+import { InputWrapper } from './styles';
 
 export interface EmailProps {
-    /**
-     *  inheritence of extension
-     */
-    extensions?: Extension[];
     /**
      * inheritence of EmailValue
      */
@@ -36,83 +17,28 @@ export interface EmailProps {
      * props of input
      */
     inputProps?: InputProps;
-    /**
-     * select props
-     */
-    selectProps?: SelectProps;
     name?: string;
     hasValidationError?: boolean;
     disabled?: boolean;
 }
-
-const defaultExtensions: Extension[] = [
-    {
-        name: 'gmail.com',
-        value: 'gmail.com',
-    },
-    {
-        name: 'hotmail.com',
-        value: 'hotmail.com',
-    },
-    {
-        name: 'yahoo.com',
-        value: 'yahoo.com',
-    },
-    {
-        name: 'outlook.com',
-        value: 'outlook.com',
-    },
-];
 
 const EmailInput = (
     {
         name,
         value,
         onChange,
-        extensions = defaultExtensions,
         inputProps = {},
-        selectProps = {},
         hasValidationError,
         disabled,
     }: EmailProps,
     ref: RefObject<HTMLInputElement>,
 ): JSX.Element => {
     const [email, setEmail] = useState<string | undefined>(value || '');
-    const [extension, setExtension] = useState<Extension['value']>('');
-    const [isDefaultValueSetted, setIsDefaultValueSetted] = useState(false);
-    const isFirstTime = useIsMountFirstTime();
-
-    const triggerChange = () => {
-        if (email) {
-            const mergeEmail = `${email}@${extension || ''}`;
-            onChange?.(mergeEmail);
-        } else {
-            onChange?.(undefined);
-        }
-    };
 
     const handleChange = (val: string) => {
         setEmail(val);
+        onChange?.(val);
     };
-
-    const onExtensionChange = (newExtension: any) => {
-        setExtension(newExtension);
-    };
-
-    useEffect(() => {
-        if (!isFirstTime) {
-            triggerChange();
-        }
-    }, [email, extension]);
-
-    useEffect(() => {
-        if (!isDefaultValueSetted && value) {
-            const [initialEmail, initialExtension] = value?.split('@');
-            setEmail(initialEmail);
-            setExtension(initialExtension);
-            setIsDefaultValueSetted(true);
-        }
-    }, [value]);
 
     return (
         <Space size="xxsmall" inline={false}>
@@ -121,32 +47,13 @@ const EmailInput = (
                     disabled={disabled}
                     ref={ref}
                     name={name}
-                    type="text"
+                    type="email"
                     value={email}
                     onChange={handleChange}
                     {...inputProps}
                     hasValidationError={hasValidationError}
                 />
             </InputWrapper>
-            <FixedWrapper>
-                <Input placeholder="@" disabled={!inputProps.disabled} />
-            </FixedWrapper>
-            <SelectWrapper>
-                <Select
-                    disabled={disabled}
-                    allowClear
-                    mode="combobox"
-                    value={extension}
-                    onChange={onExtensionChange}
-                    hasValidationError={hasValidationError}
-                    {...selectProps}>
-                    {extensions.map((curr) => (
-                        <Option key={curr.value} value={curr.value}>
-                            {curr.name}
-                        </Option>
-                    ))}
-                </Select>
-            </SelectWrapper>
         </Space>
     );
 };
