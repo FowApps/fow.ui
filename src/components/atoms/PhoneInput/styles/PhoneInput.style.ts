9fbfd94fb8f752eb257844535ea78aa460471374
alikaraarslan <alikraarslan@gmail.com>
2023-03-01 09:57:06 +0300

    💚 fix phone input style border err

--- src/components/atoms/PhoneInput/styles/PhoneInput.style.ts
+++ src/components/atoms/PhoneInput/styles/PhoneInput.style.ts
@@ -12,32 +12,13 @@ export const Wrapper = styled.div<PhoneInputProps>`
     border-radius: 4px;
     width: 100%;
     transition: all 0.3s ease;
-    border: 1px solid
-        ${(props) =>
-            props.hasValidationError
-                ? props.theme.fow.colors.error.main
-                : props.theme.fow.colors.grey.transparent32};
-
-    :hover {
-        ${(props) =>
-            !props.disabled &&
-            css`
-                border: 1px solid
-                        props.hasValidationError
-                            ? props.theme.fow.colors.error.main
-                            : props.theme.fow.colors.error.transparent48};
-            `}
-    }
+    border: none;
 
     ${(props) =>
         props.isFocused &&
         css`
             box-shadow: 0px 0px 0px 4px
-                ${props.theme.fow.colors.primary.transparent12};
-            border: 1px solid
-                ${props.hasValidationError
-                    ? props.theme.fow.colors.error.main
-                    : props.theme.fow.colors.primary.transparent48};
+                ${props.theme.fow.colors.primary.transparent12}};
         `}
 
     .react-tel-input {
@@ -49,37 +30,6 @@ export const Wrapper = styled.div<PhoneInputProps>`
 
             &[disabled] {
                 background-color: #fafafa;
-            }
-        }
-    }
-
-    .flag-dropdown {
-        border: none;
-        border-radius: 0;
-        background-color: #f5f5f5;
-
-        .selected-flag,
-        .selected-flag.open,
-        .selected-flag:hover {
-            background-color: #f5f5f5 !important;
-        }
-
-        .country-list {
-            box-shadow: ${(props) => props.theme.fow.shadows.z16};
-            border-radius: 4px;
-
-            .search {
-                padding: 10px 10px 6px 10px;
-
-                .search-emoji {
-                    display: none;
-                }
-
-                .search-box {
-                    width: 100%;
-                    margin: 0;
-                    padding: 4px 8px;
-                }
             }
         }
     }
